df11b1836b25778af3d047803aca2234e90d80f8
Zo Nirina <tomzonirina@gmail.com>
2022-12-15 14:11:19 +0300

    I change a bit thing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 export default function App() {
     return (
         <>
-            <nav class="navbar bg-light">
-                <div class="container-fluid">
+            <nav className="navbar bg-light">
+                <div className="container-fluid">
                     <a href="#" class="navbar-brand">Navbar</a>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
             </nav>
