13ccbdbec228323d8564ce3aeec4b4fd94e303bc
Zo Nirina <tomzonirina@gmail.com>
2022-12-16 05:16:54 +0300

    formular was included

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import Navbar from "./components/navbar";
+import Form from "./components/form";
 
 export default function App() {
     return(
         <>
             <Navbar/>
+            <Form/>
         </>
     )
 }
